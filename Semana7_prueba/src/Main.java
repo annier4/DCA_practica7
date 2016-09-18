b74247c8557888a50537f0c734684d43ebb7123c
Usuario <Usuario@Usuario-PC>
2016-09-18 11:58:53 -0500

    Creacion de PApplet (Prueba)

--- Semana7_prueba/src/Main.java
+++ Semana7_prueba/src/Main.java
@@ -1,9 +1,18 @@
+import processing.core.PApplet;
 
-public class Main {
+public class Main extends PApplet{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		PApplet.main("Main");
 
+	}
+	
+	static PApplet app;
+	
+	
+	
+	public void settings() {
+	
 	}
 
 }
