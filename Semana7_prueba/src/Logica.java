17f8f2a3927681042f04e8c6422e1a50a3a090cb
Usuario <Usuario@Usuario-PC>
2016-09-20 21:37:14 -0500

    Inclusion de metodo para obtener nombres con extension, FilenameFilter

--- Semana7_prueba/src/Logica.java
+++ Semana7_prueba/src/Logica.java
@@ -1,12 +1,26 @@
+import java.io.File;
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 public class Logica {
 	
-	PApplet app = Main.app;
+	PApplet app = Main.app;	
 	String ruta = "data/images/";
 	
 	public Logica() {
-	
+
+		ArrayList<String> exts =  new ArrayList<>();
+		exts.add(".png");
+		exts.add(".jpg");
+		exts.add(".bmp");		
+		File carpeta = new File(ruta);
+		String[] nombres = carpeta.list(new MiFiltroDeNombres(exts));
+		
+		for (int i = 0; i < nombres.length; i++) {
+			System.out.println(nombres[i]);
+		}
+		
 	}
 	
 	
